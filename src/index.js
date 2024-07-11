c8c34c7343764db4c18759f191ea1b933a8017ef
jheel-phantom <jhljheel@gmail.com>
2024-07-11 18:43:45 +0530

    proxy test

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.enable('trust-proxy');
 
 app.use(cors({origin:'https://captcha-test-3b172.web.app',credentials:true}));
 
@@ -19,10 +20,10 @@ app.use(session({
     resave:false,
     cookie:{
         path:"/",
-        signed:true,
         sameSite:"none",
         secure:true,
-        httpOnly:true
+        httpOnly:true,
+        
     }
 }))
 
