9210212c987cb88fc793e65051be9a8be7114b9d
jheel-phantom <jhljheel@gmail.com>
2024-07-15 13:00:04 +0530

    added dummy endpoint for aws captcha poc

---
+++ src/apis/auth/controllers/login-aws.js
@@ -0,0 +1,19 @@
+import create_api_response from "../../../common_services/api_response/create_api_response.js";
+
+const loginAWS = async (req,res) => {
+    try{
+
+        const {username, password} = req.body;
+        console.log(username,password);
+
+        //db transaction
+        //otp generation
+
+        return res.status(200).json(create_api_response(true));
+
+    }catch(err){
+        return res.status(400).json(create_api_response(false,err.message));
+    }
+}
+
+export default loginAWS;
--- src/apis/auth/index.js
+++ src/apis/auth/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import login from "./controllers/login.js";
 import loginCustom from "./controllers/login-custom.js";
+import loginAWS from "./controllers/login-aws.js";
 
 const authRouter = Router();
 
 authRouter.post("/login",login);
 authRouter.post("/login-custom",loginCustom);
+authRouter.post('/login-aws',loginAWS);
 
 export default authRouter;
